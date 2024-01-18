727fea40861f59e2595d6027edd80744c7ee14f6
Lokesh Rajendran <mexican.dirtyfellow@gmail.com>
2024-01-18 23:43:40 +0530

    move storybook config

--- packs/storybook/setup/main.ts
+++ packs/storybook/setup/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/html-vite";
 
 const config: StorybookConfig = {
-  stories: ["../packs/stories/**/*.mdx", "../packs/stories/**/*.stories.@(ts|tsx)"],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
